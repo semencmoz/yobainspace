125be30b063c0618a8ed682e44808c2df2974c75
semencmoz <semencmoz@gmail.com>
2019-10-29 15:45:28 +0300

    text inject alpha 9

--- js/headbearer.js
+++ js/headbearer.js
@@ -3,25 +3,22 @@ $(function(){
 });
 
 function reLoadGrid(gridname, dynelemcount){//загрузить или перезагрузить пустую сетку, по окончанию вставить в сетку дочерние файлы
-	$('#1').load(gridname, null, function(dynelemcount){
-		console.log("loading started ");
-		for (i=1;i<(dynelemcount+1);i++){
-			element = $('#dyn'+i);
-			element.load("/srcs/views/"+element.attr("filename"),null,function(){
-				console.log(element.attr("filename")+" loaded ");
-			});
-		}
+	$('#1').load(gridname, null, function(response, status, xhr){
+		console.log("grid load status: "+status);
+		processGrid(dynelemcount);
 	});
 }
 
-/*
 function processGrid(dynelemcount){//вставить в сетку дочерние файлы
 	for (i=1;i<(dynelemcount+1);i++){
 		element = $('#dyn'+i);
-		element.load("/srcs/views/"+element.attr("filename"));
+		element.load("/srcs/views/"+element.attr("filename"), null, function(response, status, xhr){
+		console.log("elements load status: "+status);
+		});
 	}
 }
 
+/*
 function loadGrid(gridname){//загрузить или перезагрузить пустую сетку
 	$('#1').load(gridname);
 }
