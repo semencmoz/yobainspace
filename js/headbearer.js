303b27f22769e43ac60a44a96809d9b8877e789c
semencmoz <semencmoz@gmail.com>
2019-10-29 14:21:39 +0300

    file injection alpha 4

--- js/headbearer.js
+++ js/headbearer.js
@@ -1,13 +1,16 @@
 $(function(){
-	reloadGrid("/srcs/templates/gridmain.html",7);
+	loadGrid("/srcs/templates/gridmain.html");
+	processGrid(7);
 });
 
-function reloadGrid(gridname, dynelemcount){
+function loadGrid(gridname){
 	$('#1').load(gridname);
-		for (i=1;i<(dynelemcount+1);i++){
-			element = $('#dyn'+i);
-			element.load("/srcs/views/"+element.attr("filename"));
-		}
+}
+function processGrid(dynelemcount){
+	for (i=1;i<(dynelemcount+1);i++){
+		element = $('#dyn'+i);
+		element.load("/srcs/views/"+element.attr("filename"));
+	}
 }
 
 /*import('kickass.js').then(module => {
