f36ea09629635dce8173cc2924543f32de84cfb2
semencmoz <semencmoz@gmail.com>
2019-10-29 14:27:18 +0300

    file injection alpha 5

--- js/headbearer.js
+++ js/headbearer.js
@@ -1,12 +1,16 @@
 $(function(){
-	loadGrid("/srcs/templates/gridmain.html");
-	processGrid(7);
+	reLoadGrid("/srcs/templates/gridmain.html",7);
 });
 
-function loadGrid(gridname){
+function loadGrid(gridname){//загрузить или перезагрузить пустую сетку
 	$('#1').load(gridname);
 }
-function processGrid(dynelemcount){
+
+function reLoadGrid(gridname,dynelemcount){//загрузить или перезагрузить пустую сетку, по окончанию вставить в сетку дочерние файлы
+	$('#1').load(gridname).done(processGrid(dynelemcount));
+}
+
+function processGrid(dynelemcount){//вставить в сетку дочерние файлы
 	for (i=1;i<(dynelemcount+1);i++){
 		element = $('#dyn'+i);
 		element.load("/srcs/views/"+element.attr("filename"));
