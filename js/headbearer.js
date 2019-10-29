b1d31c4b797ca7cba09d446ab924dc949f1a15c6
semencmoz <semencmoz@gmail.com>
2019-10-29 13:36:42 +0300

    html injection alpha 1

--- js/headbearer.js
+++ js/headbearer.js
@@ -1,3 +1,8 @@
-import('kickass.js').then(module => {
+$(function(){
+	$('#1').load("/srcs/templates/gridmain.html");
+});
+
+
+/*import('kickass.js').then(module => {
 	alert(module.readfile("/srcs/planedata.yb"));
-})
+})*/
