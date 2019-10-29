30e7fd930bb24dac8c7295f683c5a2e988b7d04f
semencmoz <semencmoz@gmail.com>
2019-10-29 14:09:00 +0300

    file injection alpha 3

--- js/headbearer.js
+++ js/headbearer.js
@@ -1,11 +1,14 @@
 $(function(){
-	$('#1').load("/srcs/templates/gridmain.html");
-	for (i=1;i<8;i++){
-		element = $('#dyn'+i);
-		element.load("/srcs/views/"+element.attr("filename"));
-	}
+	reloadGrid("/srcs/templates/gridmain.html",7);
 });
 
+function reloadGrid(gridname, dynelemcount){
+	$('#1').load(gridname);
+		for (i=1;i<(dynelemcount+1);i++){
+			element = $('#dyn'+i);
+			element.load("/srcs/views/"+element.attr("filename"));
+		}
+}
 
 /*import('kickass.js').then(module => {
 	alert(module.readfile("/srcs/planedata.yb"));
